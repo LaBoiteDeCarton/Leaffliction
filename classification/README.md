c86d7dbfb1bbe7e884ead59be62a1e002e532ea0
asebrech <alois.sebrechts@gmail.com>
2024-05-24 16:04:27 +0200

    predict in done

---
+++ classification/README.md
@@ -0,0 +1,69 @@
+---
+
+# Project Name
+
+## Overview
+
+This project aims to develop a convolutional neural network (CNN) for image classification using TensorFlow and Keras. The CNN architecture is designed to classify images into different categories, such as apples and grapes.
+
+## Requirements
+
+Ensure you have the necessary dependencies installed. You can install them using the following command:
+
+```bash
+pip install -r requirements.txt
+```
+
+## Overview
+
+The project consists of several components:
+
+- **Data Preprocessing**: The `data_preprocessing.py` script prepares the image data for training by scaling the pixel values and creating TensorFlow datasets.
+- **Data Augmentation**: The `data_augmentation.py` script augments the training data to improve model generalization by applying random rotations, brightness adjustments, and contrast changes to the images.
+- **Convolutional Neural Network (CNN)**: The `src/cnn.py` module defines the architecture of the convolutional neural network used for classification. It includes convolutional layers followed by max-pooling, batch normalization, dropout, and fully connected layers.
+- **Model Training**: The `train_model.py` script trains the CNN model using the augmented training data and evaluates it on the validation set. It utilizes early stopping and model checkpointing to prevent overfitting and save the best-performing model.
+- **Prediction**: The `predict.py` script takes an input image and predicts the class of leaf affliction using the trained model. It preprocesses the input image and visualizes the prediction with confidence scores.
+
+## Usage
+
+**Data Preparation**: Ensure your dataset is organized in the following structure:
+   ```
+   dataset
+   ├── class1
+   │   ├── image1.jpg
+   │   └── image2.jpg
+   ├── class2
+   │   ├── image1.jpg
+   │   └── image2.jpg
+   └── ...
+   ```
+
+To train the model:
+
+```
+python train.py <directory> [--max_images MAX_IMAGES] [--max_images_validation MAX_IMAGES_VALIDATION] [--max_images_test MAX_IMAGES_TEST]
+```
+
+- `<directory>`: Path to the directory containing the image data.
+- `--max_images`: Maximum number of images for training (default: 512).
+- `--max_images_validation`: Maximum number of images for validation (default: 64).
+- `--max_images_test`: Maximum number of images for testing (default: 64).
+
+To predict leaf affliction from an image:
+
+```
+python predict.py <image_path>
+```
+
+- `<image_path>`: Path to the image to be predicted.
+
+## Convolutional Neural Network (CNN) Architecture
+The CNN architecture used for image classification consists of multiple convolutional layers followed by fully connected layers. The architecture is designed to automatically learn features from input images and classify them into different categories. The key components of the CNN architecture include:
+
+- **Convolutional Layers**: These layers apply convolutional filters to the input images to extract features such as edges, textures, and patterns.
+- **Activation Functions**: Activation functions such as ReLU (Rectified Linear Unit) are used to introduce non-linearity into the model, allowing it to learn complex relationships between features.
+- **Pooling Layers**: Pooling layers downsample the feature maps obtained from convolutional layers, reducing the spatial dimensions and the number of parameters in the model.
+- **Dropout**: Dropout layers randomly drop a fraction of neurons during training to prevent overfitting and improve generalization.
+- **Fully Connected Layers**: These layers receive flattened feature maps from the convolutional layers and perform classification based on the learned features.
+
+---
--- classification/predict.py
+++ classification/predict.py
@@ -5,11 +5,10 @@ from PIL import Image
 from keras.models import load_model
 import cv2
 import numpy as np
-
 from matplotlib import pyplot as plt
-import numpy as np
 
 from src.data_preprocessing import get_preprocessing
+
 
 def check_image_arg(image):
     if not os.path.exists(image):
@@ -17,15 +16,22 @@ def check_image_arg(image):
     if not image.endswith(('.jpeg', '.jpg', '.png')):
         raise ValueError("Image must be a valid image file")
 
+
 def check_augmented_images_directory():
     try:
-        image_files = glob.glob('learnings/augmented_images/**/*.jpeg', recursive=True)
-        image_files += glob.glob('learnings/augmented_images/**/*.jpg', recursive=True)
-        image_files += glob.glob('learnings/augmented_images/**/*.png', recursive=True)
+        image_files = glob.glob('learnings/augmented_images/**/*.jpeg',
+                                recursive=True)
+        image_files += glob.glob('learnings/augmented_images/**/*.jpg',
+                                 recursive=True)
+        image_files += glob.glob('learnings/augmented_images/**/*.png',
+                                 recursive=True)
         if not image_files:
             raise ValueError("Images must be valid image files")
     except Exception as e:
-        raise ValueError(f"something is wrong with the augmented images directory: {str(e)}")
+        err = ("something is wrong with the augmented ",
+               f"images directory: {str(e)}")
+        raise ValueError(err)
+
 
 def check_models_directory():
     if not os.path.exists('learnings/models'):
@@ -35,6 +41,7 @@ def check_models_directory():
     for model in os.listdir('learnings/models'):
         if not model.endswith('.keras'):
             raise ValueError("Models must be keras files")
+
 
 def check_args(args):
     try:
@@ -47,66 +54,69 @@ def check_args(args):
 
 
 def process_image_to_white_bg(image):
-    # Convert the image to grayscale
     grayscale = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2GRAY)
-    
-    # Apply a binary threshold to the image
     _, binary = cv2.threshold(grayscale, 127, 255, cv2.THRESH_BINARY_INV)
-    
-    # Invert the binary image to get the mask
     mask = cv2.bitwise_not(binary)
-    
-    # Convert the mask to three channels
     mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
-    
-    # Use the mask to set the background of the original image to white
-    result = np.where(mask==np.array([255, 255, 255]), np.array([255, 255, 255]), image)
-    
-    # Convert the result to an 8-bit unsigned integer array
+    result = np.where(mask == np.array([255, 255, 255]),
+                      np.array([255, 255, 255]), image)
     result = result.astype(np.uint8)
-    
+
     return Image.fromarray(result)
 
 
 def plot_classification(original_image_path, predicted_class):
-    # Load the original image
     original_image = Image.open(original_image_path)
-    
-    # Process the image to have a white background
     processed_image = process_image_to_white_bg(original_image)
 
-    # Create a plot with 2 subplots
     fig, axes = plt.subplots(1, 2, figsize=(10, 5))
-
-    # Set the background color to black
     fig.patch.set_facecolor('black')
 
-    # Display the original image
     axes[0].imshow(original_image)
     axes[0].axis('off')
-    
-    # Display the processed image
+
     axes[1].imshow(processed_image)
     axes[1].axis('off')
 
-    # Add the predicted class annotation
-    # Move the title to the bottom
     title = 'DL classification'
     predicted_text = f'Class predicted: {predicted_class}'
-    title = f'{"="*3}{" "*49}{title}{" "*49}{"="*3}'
+    title = f'{"="*3}{" "*45}{title}{" "*45}{"="*3}'
     full_text = f'{title}\n\n{predicted_text}'
     plt.figtext(0.5, 0.01, full_text, ha="center", fontsize=16, color='white')
-    # Adjust layout and show the plot
+
     plt.tight_layout()
-    plt.subplots_adjust(bottom=0.15)  # Adjust space for the title
+    plt.subplots_adjust(bottom=0.15)
     plt.show()
 
 
 def predict(args):
-    model = load_model('learnings/models/Apple_model.keras')
+    main_classe = args.image.rstrip('/')
+    if 'Apple' in main_classe:
+        main_classe = 'Apple'
+    elif 'Grappe' in main_classe:
+        main_classe = 'Grappe'
+    else:
+        err = ("please choose a path that contains 'Apple' or 'Grappe' ",
+               "in the path.")
+        raise ValueError(err)
+    if not os.path.exists(f'learnings/augmented_images/{main_classe}'):
+        err = (f"Directory 'learnings/augmented_images/{main_classe}' ",
+               "does not exist")
+        raise ValueError(err)
+    model = load_model(f'learnings/models/{main_classe}_model.keras')
+
+    path = f'learnings/augmented_images/{main_classe}/validation'
+    validation = get_preprocessing(path)
+    preds = model.predict(validation)
+    val_loss, val_acc = model.evaluate(validation)
+
+    print('\nValidation Loss: ', val_loss)
+    print('\nValidation Accuracy: ', np.round(val_acc * 100), '%')
+
     original_image = Image.open(args.image)
-    preds = model.predict(np.expand_dims(original_image, axis = 0))
-    labels = os.listdir('learnings/augmented_images/Apple/validation')
+    preprocessed_image = np.array(original_image) / 255.0
+    preds = model.predict(np.expand_dims(preprocessed_image, axis=0))
+    labels = os.listdir(f'learnings/augmented_images/{main_classe}/validation')
 
     preds_class = np.argmax(preds)
     preds_label = labels[preds_class]
@@ -114,15 +124,12 @@ def predict(args):
     print(f'\nPredicted Class: {preds_label}')
     print(f'\nConfidence Score: {preds[0][preds_class]}')
 
-    # plt.imshow(original_image)
-    # plt.title(f'Predicted Class: {preds_label}')
-    # plt.show()    
-    plot_classification(args.image, preds_class)
+    plot_classification(args.image, preds_label)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Predict a model.')
-    parser.add_argument("image", type=str, help='Image to predict')
+    parser.add_argument("image", type=str, help='Path to the image to predict')
     args = parser.parse_args()
     check_args(args)
     predict(args)
