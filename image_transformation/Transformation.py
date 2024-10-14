5621df4e4a8e2cffa41e073580317e57dff42797
LaBoiteDeCarton <daniel.mercadiel@gmail.com>
2024-10-14 18:53:23 +0200

    plot on show arg. Histogramm update

--- image_transformation/Transformation.py
+++ image_transformation/Transformation.py
@@ -3,9 +3,7 @@ import os
 from plantcv import plantcv as pcv
 from pathlib import Path
 import matplotlib.pyplot as plt
-
-#plt.rcParams["figure.max_open_warning"] = False
-pcv.params.debug = "plot"
+import pandas as pd
 
 class Transformation:
     bin_mask = None
@@ -45,7 +43,7 @@ class Transformation:
             raise ValueError("Image is not set")
         if self.bin_mask is None:
             self.create_binary_mask()
-        #height of the image
+        return pcv.roi.from_binary_image(self.image, self.bin_mask)
         h, w, _ = self.image.shape
         roi_rect = pcv.roi.rectangle(self.image, x=0, y=0, h=h, w=w)
         # Make a new filtered mask that only keeps the plants in your ROI and not objects outside of the ROI
@@ -101,6 +99,76 @@ class Transformation:
         # plt.show()       
         # print(top, bottom, center_v, left, right, center_h)
 
+    def old_transform_histogram(self):
+        if self.image is None:
+            raise ValueError("Image is not set")
+        _, hist_df = pcv.visualize.histogram(img=self.image, bins=255, hist_data=True)
+        l = pcv.rgb2gray_lab(rgb_img=self.image, channel='l')
+        a = pcv.rgb2gray_lab(rgb_img=self.image, channel='a')
+        b = pcv.rgb2gray_lab(rgb_img=self.image, channel='b')
+        h = pcv.rgb2gray_hsv(rgb_img=self.image, channel='h')
+        s = pcv.rgb2gray_hsv(rgb_img=self.image, channel='s')
+        v = pcv.rgb2gray_hsv(rgb_img=self.image, channel='v')
+        _, l_hist_df = pcv.visualize.histogram(l, bins=255, hist_data=True)
+        _, a_hist_df = pcv.visualize.histogram(a, bins=255, hist_data=True)
+        _, b_hist_df = pcv.visualize.histogram(b, bins=255, hist_data=True)
+        _, h_hist_df = pcv.visualize.histogram(h, bins=255, hist_data=True)
+        _, s_hist_df = pcv.visualize.histogram(s, bins=255, hist_data=True)
+        _, v_hist_df = pcv.visualize.histogram(v, bins=255, hist_data=True)
+        hist_df = hist_df[hist_df['proportion of pixels (%)'] != 0]
+        l_hist_df = l_hist_df[l_hist_df['proportion of pixels (%)'] != 0]
+        a_hist_df = a_hist_df[a_hist_df['proportion of pixels (%)'] != 0]
+        b_hist_df = b_hist_df[b_hist_df['proportion of pixels (%)'] != 0]
+        h_hist_df = h_hist_df[h_hist_df['proportion of pixels (%)'] != 0]
+        s_hist_df = s_hist_df[s_hist_df['proportion of pixels (%)'] != 0]
+        v_hist_df = v_hist_df[v_hist_df['proportion of pixels (%)'] != 0]
+
+        plt.plot(l_hist_df['pixel intensity'], l_hist_df['proportion of pixels (%)'], label="lightness", color="darkgreen")
+        plt.plot(a_hist_df['pixel intensity'], a_hist_df['proportion of pixels (%)'], label="green-magenta", color="magenta")
+        plt.plot(b_hist_df['pixel intensity'], b_hist_df['proportion of pixels (%)'], label="blue-yellow", color="yellow")
+        plt.plot(h_hist_df['pixel intensity'], h_hist_df['proportion of pixels (%)'], label="hue", color="darkviolet")
+        plt.plot(s_hist_df['pixel intensity'], s_hist_df['proportion of pixels (%)'], label="saturation", color="lightblue")
+        plt.plot(v_hist_df['pixel intensity'], v_hist_df['proportion of pixels (%)'], label="value", color="orange")
+
+        for color in hist_df['color channel'].unique():
+            subset = hist_df[hist_df['color channel'] == color]
+            plt.plot(subset['pixel intensity'], subset['proportion of pixels (%)'], label=color, color=color)
+        
+        plt.legend()
+        plt.title('Histogram of Image')
+        plt.show()
+
+    def transform_histogram(self):
+        if self.image is None:
+            raise ValueError("Image is not set")
+
+        channels = {'h': {'color': "darkviolet", 'label': "hue"}, 's': {'color': "lightblue", 'label': "saturation"}, 'v': {'color': "orange", 'label': "value"}, 'l': {'color': "darkgreen", 'label': "lightness"}, 'a': {'color': "magenta", 'label': "green-magenta"}, 'b': {'color': "yellow", 'label': "blue-yellow"}}
+        hist_df = pd.DataFrame()
+
+        for channel in channels:
+            color = channels[channel]['color']
+            label = channels[channel]['label']
+            if channel in ['h', 's', 'v']:
+                channel_image = pcv.rgb2gray_hsv(rgb_img=self.image, channel=channel)
+            else:
+                channel_image = pcv.rgb2gray_lab(rgb_img=self.image, channel=channel)
+            _, hist_df_channel = pcv.visualize.histogram(channel_image, bins=255, hist_data=True)
+            hist_df_channel.loc[:, 'color channel'] = color
+            hist_df_channel.loc[:, 'label'] = label
+            hist_df = pd.concat([hist_df, hist_df_channel])
+        _, hist_df_rgb = pcv.visualize.histogram(img=self.image, bins=255, hist_data=True)
+        hist_df_rgb.loc[:, 'label'] = hist_df_rgb['color channel']
+        hist_df = pd.concat([hist_df, hist_df_rgb])
+        hist_df = hist_df[hist_df['proportion of pixels (%)'] != 0]
+
+        for color in hist_df['color channel'].unique():
+            subset = hist_df[hist_df['color channel'] == color]
+            label = subset['label'].iloc[0]
+            plt.plot(subset['pixel intensity'], subset['proportion of pixels (%)'], label=label, color=color)
+        plt.legend()
+        plt.title('Histogram of Image')
+        plt.show()
+
 
 def transform_gaussian_blur(image):
     #return pcv.gaussian_blur(image,ksize=(5, 5))
@@ -114,17 +182,29 @@ def file_path(path):
         return path
     else:
         raise argparse.ArgumentTypeError(f"readable_dir:{path} is not a valid path")
+    
+def dir_path(path):
+    if os.path.isdir(path):
+        return path
+    else:
+        raise argparse.ArgumentTypeError(f"dst:{path} is not a valid directory")
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Augmentation')
     parser.add_argument('path', type=file_path, help='Path to the image')
     parser.add_argument('--show', action='store_true', help='Show the augmented images')
+    parser.add_argument('--src', type=dir_path, help='Path to the source folder')
+    parser.add_argument('--dst', type=dir_path, help='Path to the destination folder')
 
     args = parser.parse_args()
 
     file_name = Path(args.path).stem
     image, _ , _ = pcv.readimage(args.path)
+
+    if args.show:
+        plt.rcParams["figure.max_open_warning"] = False
+        pcv.params.debug = "plot"
     
     transformation = Transformation(image)
     gaussian_blur_transformation, mask_transformation = transformation.subjectTransform()
@@ -138,6 +218,10 @@ if __name__ == '__main__':
     #analyse color and plot it
     #analyze_color = pcv.analyze.color(image, labeled_masks)
     labeled_masks, num_labels = pcv.create_labels(transformation.bin_mask, rois=None, roi_type="partial")  
-    histogram = pcv.analyze.color(transformation.image, labeled_masks[0], n_labels=1, colorspaces="all", label=None)
+    plotting_img = pcv.visualize.colorspaces(image, original_img=True)
+    transformation.transform_histogram()
+    # histogram = pcv.analyze.color(rgb_img=plotting_img, labeled_mask=labeled_masks, colorspaces="all")
+    
+
 
 
